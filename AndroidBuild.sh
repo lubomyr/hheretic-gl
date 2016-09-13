a7deb815bdbe390250d484788c45be4eb043f75d
lubomyr <lubomyr31@gmail.com>
2016-09-13 17:07:26 +0300

    first commit

---
+++ AndroidBuild.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+LOCAL_PATH=`dirname $0`
+LOCAL_PATH=`cd $LOCAL_PATH && pwd`
+
+ln -sf libsdl_mixer.so $LOCAL_PATH/../../../obj/local/$1/libSDL_mixer.so
+ln -sf libglshim.a $LOCAL_PATH/../../../obj/local/$1/libGL.a
+ln -sf libglu.a $LOCAL_PATH/../../../obj/local/$1/libGLU.a
+
+[ -d hheretic-0.2.3-src ] || { curl "http://netix.dl.sourceforge.net/project/hhexen/hheretic/0.2.3/hheretic-0.2.3-src.tgz" | tar zxv && patch -p0 < hheretic-0.2.3-android.diff || exit 1 ; }
+
+cd hheretic-0.2.3-src
+if [ \! -f Makefile ] ; then
+	../../setEnvironment.sh ./configure --host=arm-linux-androideabi --without-x
+fi
+rm -f ../libapplication.so
+make -j4 CPPFLAGS="-include SDL_main.h -Iinclude -I."
+mv -f hheretic-gl ../libapplication.so
