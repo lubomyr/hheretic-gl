668625ebcd1b8b8fec3388f07fbd792448c807f2
lubomyr <lubomyr31@gmail.com>
2016-11-06 16:47:07 +0200

    glshim library renamed to gl4es

--- AndroidBuild.sh
+++ AndroidBuild.sh
@@ -4,7 +4,7 @@ LOCAL_PATH=`dirname $0`
 LOCAL_PATH=`cd $LOCAL_PATH && pwd`
 
 ln -sf libsdl_mixer.so $LOCAL_PATH/../../../obj/local/$1/libSDL_mixer.so
-ln -sf libglshim.a $LOCAL_PATH/../../../obj/local/$1/libGL.a
+ln -sf libgl4es.a $LOCAL_PATH/../../../obj/local/$1/libGL.a
 ln -sf libglu.a $LOCAL_PATH/../../../obj/local/$1/libGLU.a
 
 [ -d hheretic-0.2.3-src ] || { curl "http://netix.dl.sourceforge.net/project/hhexen/hheretic/0.2.3/hheretic-0.2.3-src.tgz" | tar zxv && patch -p0 < hheretic-0.2.3-android.diff || exit 1 ; }
